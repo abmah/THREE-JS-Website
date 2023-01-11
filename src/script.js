7311a3f0645c874f260ac784ac83fa9d727b5716
abmah <ab.mahasnh@gmail.com>
2023-01-11 21:22:35 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,190 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+/**
+ * Base
+ */
+// Debug
+// const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+
+let mixer = null;
+let prevScrollPos = 0;
+
+gltfLoader.load(
+    '/models/pls.glb',
+    (gltf) => {
+        mixer = new THREE.AnimationMixer(gltf.scene);
+
+        // Iterate through the animations array
+        for (let i = 0; i < gltf.animations.length; i++) {
+            // Get the animation clip
+            const clip = gltf.animations[i];
+
+            // Create an action for the animation
+            const action = mixer.clipAction(clip);
+
+            // Set the time scale to 1 to play the animation forward
+
+
+            // Play the animation
+            action.play();
+        }
+        gltf.scene.scale.set(0.5, 0.5, 0.5);
+        scene.add(gltf.scene);
+    },
+);
+
+const animationDuration = 0.8333333134651184;
+const animationLength = 3000; // total length of animation in pixels
+let prevTime = performance.now();
+
+window.addEventListener('scroll', () => {
+    // Get the current scroll position and time
+    const scrollPos = Math.abs(window.scrollY + 1);
+    const time = performance.now();
+
+    // Compute the time delta based on the animation duration and scroll delta
+    const delta = (scrollPos - prevScrollPos) / animationLength * animationDuration;
+    prevTime = time;
+
+    // Update the animation with the time delta
+    if (mixer) {
+        mixer.update(delta);
+    }
+
+    // Update the previous scroll position
+    prevScrollPos = scrollPos;
+});
+/**
+ * Floor
+ */
+// const floor = new THREE.Mesh(
+//     new THREE.PlaneGeometry(10, 10),
+//     new THREE.MeshStandardMaterial({
+//         color: '#444444',
+//         metalness: 0,
+//         roughness: 0.5
+//     })
+// )
+// floor.receiveShadow = true
+// floor.rotation.x = - Math.PI * 0.5
+// scene.add(floor)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+
+
+// const pointLight = new THREE.PointLight(
+//     0xffffff,
+//     0.6,
+//     10
+
+// )
+// pointLight.position.set(3, 3, 3)
+// scene.add(pointLight)
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(2, 2, 2)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 0.75, 0)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    // Update controls
+    controls.update()
+
+    // Mixer update
+    // if (mixer) {
+    //     mixer.update(deltaTime)
+    // }
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
